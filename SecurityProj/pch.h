8a163a24dba6dc7ebd4b82114f954ad80e937710
mh991221 <39687014+mh991221@users.noreply.github.com>
2024-12-02 18:01:09 +0900

    db

---
+++ SecurityProj/pch.h
@@ -0,0 +1,5 @@
+#ifdef _DEBUG
+#pragma comment(lib, "MySQL\\Debug\\mysqlcppconn.lib")
+#else
+#pragma comment(lib, "MySQL\\Release\\mysqlcppconn.lib")
+#endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
+#include <stdlib.h>
+#include <iostream>
 #include "Server.h"
 #include "Client.h"
+#include "mysql_driver.h" 
+#include "mysql_connection.h"
+
 #include <thread>
 #include <chrono>
+
+#include <mysql.h>
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/prepared_statement.h>
+
+using namespace std;
+
+#ifdef _DEBUG
+#pragma comment(lib, "mysqlcppconn.lib")
+#else
+#pragma comment(lib, "MySQL\\Release\\mysqlcppconn.lib")
+#endif
+
 
 int main() {
     // Start the server in a separate thread
@@ -13,8 +32,81 @@ int main() {
     // Start the client
     startClient();
 
+    sql::mysql::MySQL_Driver* driver;
+
+
+    try {
+        // MySQL 드라이버 인스턴스를 가져옵니다.
+        driver = sql::mysql::get_driver_instance();
+
+        // 데이터베이스에 연결합니다.
+        const string server = "tcp://127.0.0.1:3306";
+        const string name = "root";
+        const string password = "dlaalsgur5162!";
+        std::unique_ptr<sql::Connection> conn(driver->connect(server, name, password));
+
+        // 데이터베이스 작업을 수행합니다.
+        conn->setSchema("securityproj");
+
+        // 쿼리 실행
+        std::unique_ptr<sql::Statement> stmt(conn->createStatement());
+
+        stmt->execute("create table if not exists department(departmentName varchar(20) not null,\
+                                                              revenue bigint null,\
+                                                              CostofGoodsSold bigint null,\
+                                                              OperatingExpenses bigint null,\
+                                                              NonOperatingExpenses bigint null,\
+                                                              CurrentAssets bigint null,\
+                                                              NonCurrentAssets bigint null,\
+                                                              CurrentLiability bigint null,\
+                                                              NonCurrentLiability bigint null,\
+                                                              CapitalStock bigint null,\
+                                                              RetainedEarning bigint null\
+                                                              )");
+
+
+        stmt->execute("create table if not exists analyzedData(departmentName varchar(20) not null,\
+                                                              GrossProfit bigint null,\
+                                                              OperatingProfit bigint null,\
+                                                              NetProfit bigint null,\
+                                                              TotalAssets bigint null, \
+                                                              TotalLiabilities bigint null,\
+                                                              TotalEquity bigint null\
+                                                              )");
+                                                              
+
+        stmt->execute("create table if not exists departmentKey(departmentName varchar(20) not null,\
+                                                              publicKey bigint null\
+                                                              )");
+
+
+        std::unique_ptr<sql::ResultSet> department(stmt->executeQuery("SELECT * FROM department"));
+        std::unique_ptr<sql::ResultSet> deparmentKey(stmt->executeQuery("SELECT * FROM departmentkey"));
+        std::unique_ptr<sql::ResultSet> analyzedData(stmt->executeQuery("SELECT * FROM analyzeddata"));
+
+        // 결과 처리
+        while (department && department->next()) {
+            std::cout << department->getString(1) << " " << department->getString(2) << std::endl;
+        }
+
+    }
+    catch (sql::SQLException& e) {
+        std::cerr << "SQLException: " << e.what() << std::endl;
+        std::cerr << "Error code: " << e.getErrorCode() << std::endl;
+        std::cerr << "SQL state: " << e.getSQLState() << std::endl;
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    catch (...) {
+        std::cerr << "Unknown exception occurred" << std::endl;
+    }
+
     // Wait for the server thread to finish
     serverThread.join();
+    
 
     return 0;
 }
+    
+
